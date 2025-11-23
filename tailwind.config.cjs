e231c2769e043c6d9750cb4bf5c398776223a79d
Nguyễn Duy Đạt <duydat10122001@gmail.com>
2025-11-23 16:28:27 +0700

    update

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./**/*.html",
+    "./**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#10b981',
+        secondary: '#f97316',
+        'dark-bg': '#0f172a',
+        'light-bg': '#ffffff',
+        'light-text': '#f1f5f9',
+        'dark-text': '#0f172a',
+      }
+    }
+  },
+  plugins: [],
+}
